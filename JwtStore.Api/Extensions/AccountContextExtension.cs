6c5f935a3f7a0bdb5070872b3774a35301b6064c
Ivan Clay <ivan.clay.moura@gmail.com>
2023-09-24 09:00:21 -0300

    authenticate

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -15,6 +15,13 @@ public static class AccountContextExtension
             JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IService,
             JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>();
         #endregion
+
+        #region Authenticate
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts.IRepository,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Authenticate.Repository>();
+
+        #endregion
     }
 
     public static void MapAccountEndpoints(this WebApplication app) 
@@ -31,6 +38,26 @@ public static class AccountContextExtension
             return result.IsSuccess
             ? Results.Created($"api/v1/users{result.Data?.Id}", result)
             : Results.Json(result, statusCode: result.Status);
+        });
+        #endregion
+
+        #region Authenticate
+        app.MapPost("api/v1/authenticate", async (
+            JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Request request,
+            IRequestHandler<
+                JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Request,
+                JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Response> handler
+            ) =>
+        {
+            var result = await handler.Handle(request, new CancellationToken());
+            if(!result.IsSuccess)
+                return Results.Json(result, statusCode: result.Status);
+
+            if(result.Data is null)
+                return Results.Json(result, statusCode:500);
+
+            result.Data.Token = JwtExtension.Generate(result.Data);
+            return Results.Ok(result);
         });
         #endregion
     }
